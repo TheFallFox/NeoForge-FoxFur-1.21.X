97a22b75c28f9dda78198cb96ec8fea4038c5903
ttfender <ttfender@iu.edu>
2024-11-14 21:19:48 -0500

    Getting Directory Ready

---
+++ src/main/java/net/foxel/foxfur/item/ModCreativeModeTabs.java
@@ -0,0 +1,12 @@
+package net.foxel.foxfur.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.item.CreativeModeTab;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModCreativeModeTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TAB =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB,)
+
+
+}
