c9f06faa1b8b752b91242e1ab51a6aba95e4646e
Foxel <0lthefallfoxl0@gmail.com>
2024-11-22 23:38:39 -0500

    Commit

--- src/main/java/net/foxel/foxfur/block/custom/Air_Hockey_Table.java
+++ src/main/java/net/foxel/foxfur/block/custom/Air_Hockey_Table.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HalfTransparentBlock;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -12,7 +13,6 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 public class Air_Hockey_Table extends HalfTransparentBlock {
 
     public static final VoxelShape SHAPE = Block.box(-5, 8, -18, 21, 16, 34);
-
 
     public Air_Hockey_Table(Properties properties) {
         super(properties);
